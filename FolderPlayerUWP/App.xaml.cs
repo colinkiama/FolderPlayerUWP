a56df532e05a84d3a0063428a758ecc9c8b95b78
Colin Kiama <colinkiama@gmail.com>
2019-02-02 15:10:18 +0000

    title bar color changes work properly now

--- FolderPlayerUWP/App.xaml.cs
+++ FolderPlayerUWP/App.xaml.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -27,10 +30,47 @@ namespace FolderPlayerUWP
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
+        /// 
+        UISettings uiSettings = new UISettings();
+        
         public App()
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
+            uiSettings.ColorValuesChanged += ColorValuesChanged;
+        }
+
+        private async void ColorValuesChanged(UISettings sender, object args)
+        {
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                var appView = ApplicationView.GetForCurrentView();
+                var newBackground = sender.GetColorValue(UIColorType.Background);
+                UpdateTitleBar(appView, newBackground);
+                
+            });
+        }
+
+        private void UpdateTitleBar(ApplicationView appView, Color newBackground)
+        {
+            var titleBar = appView.TitleBar;
+
+            if (newBackground == Colors.White)
+            {
+                var background = Colors.White;
+                titleBar.BackgroundColor = background;
+                titleBar.ButtonBackgroundColor = background;
+                titleBar.InactiveBackgroundColor = background;
+                titleBar.ButtonInactiveBackgroundColor = background;
+            }
+            else
+            {
+                Color darkColor = Color.FromArgb(255, 25, 25, 25);
+                titleBar.BackgroundColor = darkColor;
+                titleBar.ButtonBackgroundColor = darkColor;
+                titleBar.InactiveBackgroundColor = darkColor;
+                titleBar.ButtonInactiveBackgroundColor = darkColor;
+            }
         }
 
         /// <summary>
@@ -43,6 +83,8 @@ namespace FolderPlayerUWP
             Frame rootFrame = Window.Current.Content as Frame;
             var appView = ApplicationView.GetForCurrentView();
             appView.SetPreferredMinSize(new Size(300, 400));
+            UpdateTitleBar(appView);
+           
             // Do not repeat app initialization when the Window already has content,
             // just ensure that the window is active
             if (rootFrame == null)
@@ -73,6 +115,17 @@ namespace FolderPlayerUWP
                 // Ensure the current window is active
                 Window.Current.Activate();
             }
+        }
+
+        private void UpdateTitleBar(ApplicationView appView)
+        {
+            var backgroundBrush = (SolidColorBrush)Application.Current.Resources["ApplicationPageBackgroundThemeBrush"];
+            var titleBar = appView.TitleBar;
+            titleBar.BackgroundColor = backgroundBrush.Color;
+            titleBar.ButtonBackgroundColor = backgroundBrush.Color;
+            titleBar.InactiveBackgroundColor = backgroundBrush.Color;
+            titleBar.ButtonInactiveBackgroundColor = backgroundBrush.Color;
+
         }
 
         /// <summary>
